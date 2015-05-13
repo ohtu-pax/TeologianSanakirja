64af0942b10922e14b5bb6280a12887db4537dc8
Katri Roos <katri.roos@cs.helsinki.fi>
2015-05-13 11:21:39 +0300

    Katrin ja Anskun get

--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -1,3 +1,13 @@
+
+sanakirjaApp.controller('sanalistaController', function($scope, sanakirjaAPIservice) {
+    $scope.sanalista = [];
+
+    sanakirjaAPIservice.getSanat().success(function (response) {
+        $scope.sanalista = JSON.parse(response);
+    });
+  });
+
+
 sanakirjaApp.controller('testiController', function($scope){
     $scope.title = "teologian sanakirja";
 });
