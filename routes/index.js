76137a841368661250995920d0b3335a195f4717
tmakij <timo.j.maki@helsinki.fi>
2015-05-13 10:11:04 +0300

    post & get api/sanat

--- routes/index.js
+++ routes/index.js
@@ -4,37 +4,48 @@ var pg = require('pg');
 var connectionString = "postgres://ackprmsigyevpu:Xr2OAYPV1l1GAVW6MMMtVWUhIF@ec2-23-23-81-221.compute-1.amazonaws.com:5432/d4lr4f1ndusjb?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory";
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
+router.get('/', function (req, res, next) {
+    res.render('index');
 });
 
-// var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/tixtixti';
+router.post('/api/sanat', function (req, res) {
+    var data = {sana: req.body.sana, selitys: req.body.selitys};
+    pg.connect(connectionString, function (err, client, done) {
+        var query = client.query('INSERT INTO sanat(sana, selitys) VALUES ($1, $2)', [data.sana, data.selitys]);
+        query.on('end', function () {
+            res.end();
+        });
+        if (err) {
+            console.log(err);
+        }
+    });
+});
 
-router.get('/api/tixtixti', function(req, res) {
+router.get('/api/sanat', function (req, res) {
 
-    
+
     var results = [];
 
     // Get a Postgres client from the connection pool
-    pg.connect(connectionString, function(err, client, done) {
+    pg.connect(connectionString, function (err, client, done) {
 
         // SQL Query > Select Data
-        var query = client.query("SELECT * FROM products");
+        var query = client.query("SELECT * FROM sanat");
 
         // Stream results back one row at a time
-        query.on('row', function(row) {
+        query.on('row', function (row) {
             results.push(row);
         });
 
         // After all data is returned, close connection and return results
-        query.on('end', function() {
+        query.on('end', function () {
             client.end();
             return res.json(results);
         });
 
         // Handle Errors
-        if(err) {
-          console.log(err);
+        if (err) {
+            console.log(err);
         }
 
     });
