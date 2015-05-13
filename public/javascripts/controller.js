f87fc547041ad64214b11933b120f0a5948d6c54
tixtixti <tktuononen@hotmail.com>
2015-05-13 14:54:41 +0300

    pientärefaktorointia

--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -1,18 +1,18 @@
 
-sanakirjaApp.controller('sanalistaController', function($scope, $http, sanakirjaAPIservice) {
+sanakirjaApp.controller('sanalistaController', function ($scope, $http, sanakirjaAPIservice) {
 
     // Get all todos
     $http.get('api/sanat')
-        .success(function(data) {
-            console.log(data);
-            $scope.sanalista = data;
-        })
-        .error(function(error) {
-            console.log('Error: ' + error);
-        });
+            .success(function (data) {
+                console.log(data);
+                $scope.sanalista = data;
+            })
+            .error(function (error) {
+                console.log('Error: ' + error);
+            });
 });
 
 
-sanakirjaApp.controller('testiController', function($scope){
+sanakirjaApp.controller('testiController', function ($scope) {
     $scope.title = "teologian sanakirja";
 });
