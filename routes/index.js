3dfd8e58eebbdebbd098ed146dfd0a782e7afcfc
tmakij <timo.j.maki@helsinki.fi>
2015-05-18 11:00:38 +0300

    testataan get/sanat

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@
 
 var express = require('express');
 var router = express.Router();
-var databaseStore = require('../databaseStore');
+var database = require('../database');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -11,14 +11,12 @@ router.get('/', function (req, res, next) {
 
 router.post('/api/sanat', function (req, res) {
     var data = {sana: req.body.sana, selitys: req.body.selitys};
-    var database = databaseStore.getDatabase();
     database.queryWithValues('INSERT INTO sanat(sana, selitys) VALUES ($1, $2)', [data.sana, data.selitys], function () {
         res.end();
     });
 });
 
 router.get('/api/sanat', function (req, res) {
-    var database = databaseStore.getDatabase();
     database.queryWithReturn('SELECT * FROM sanat', function (results) {
         return res.json(results);
         res.end();
--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,63 +1,42 @@
 'use strict';
 
-var assert = require("assert");
-var request = require('supertest')
-var http = require('http');
+var request = require('supertest');
 var routes = require('.././routes/index');
-var databaseStore = require('../databaseStore');
 var express = require('express');
-var router = express.Router();
+
 var app = express();
 app.use('/', routes);
 
-var server = null;
-databaseStore.setDatabase(null);
-
-function startServer()
-{
-    server = http.createServer(function (request, response) {
-        console.log(request.url);
-        server.serve(request, response);
-    }).listen(3198);
-    server.on("listening", function () {
-        console.log("Server started");
-    });
-}
-
-function closeServer() {
-    if (server !== null) {
-        server.close();
-        console.log("Server Stopped");
-    }
-}
-
 describe('api/sanat', function () {
-    before(function () {
-        console.log('Before');
-        startServer();
-    });
-    after(function () {
-        closeServer();
-        console.log('After');
-    });
-    /*beforeEach(function () {
-     console.log('beforeEach');
-     });
-     afterEach(function () {
-     console.log('afterEach');
-     });*/
-    it('respond with json', function (done) {
+    it('Palautuu JSON muodossa', function (done) {
         request(app)
                 .get('/api/sanat')
                 .set('Accept', 'application/json')
                 .expect('Content-Type', /json/)
-                .expect(200, done);
+                .expect(200)
+                .end(function (err, res) {
+                    if (err)
+                        return done(err);
+                    done();
+                });
     });
     it('Tulisi palauttaa oikeita sanoja', function (done) {
         request(app)
                 .get('/api/sanat')
                 .set('Accept', 'application/json')
-                .expect('Content-Type', /json/)
-                .expect(200, done);
+                .expect(function (res) {
+                    var results = JSON.parse(res.text);
+                    var sana = results[0];
+                    if (!sana || !sana.id || !sana.sana || !sana.selitys) {
+                        throw new Error('Virheellinen sana');
+                    }
+                    return false;
+                })
+                .end(function (err, res) {
+                    if (err) {
+                        return done(err);
+                    }
+                    done();
+                });
     });
 });
