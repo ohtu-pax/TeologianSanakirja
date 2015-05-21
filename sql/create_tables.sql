24a3a0349fa0a4bd1e5c42d338aa96d582657d11
tmakij <timo.j.maki@helsinki.fi>
2015-05-21 13:09:35 +0300

    sql tablet

---
+++ sql/create_tables.sql
@@ -0,0 +1,18 @@
+CREATE TABLE selitykset (
+    id              SERIAL PRIMARY KEY,
+    selitys         TEXT NOT NULL
+);
+
+CREATE TABLE hakusanat (
+    id              SERIAL PRIMARY KEY,
+    hakusana        TEXT NOT NULL CONSTRAINT epatyhja_hakusana  CHECK (length(hakusana) > 0),
+    selitys         INTEGER NOT NULL REFERENCES selitykset ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE linkit (
+    id              SERIAL PRIMARY KEY,
+    selitys         INTEGER NOT NULL REFERENCES selitykset ON DELETE CASCADE ON UPDATE CASCADE,
+    hakusana        INTEGER NOT NULL REFERENCES hakusanat ON DELETE CASCADE ON UPDATE CASCADE,
+    linkkisana      TEXT NOT NULL CONSTRAINT epatyhja_linkki CHECK (length(linkkisana) > 0)
+);
+
