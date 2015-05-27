8df1c78333b4243c062f3a14b4d445f907b35af1
tmakij <timo.j.maki@helsinki.fi>
2015-05-27 10:48:27 +0300

    test dataan muutoksia

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -9,10 +9,12 @@ INSERT INTO hakusanat (hakusana, selitys) VALUES
 ('Test',1),
 ('asd',2),
 ('dd',3),
+('test',4),
 ('qwe',5),
 ('Test2',1);
 
 INSERT INTO linkit (linkkisana, selitys, hakusana) VALUES
 ('2222222222222222222222222',3,1),
-('totest',4,1);
+('totest',4,1),
+('wqe',5,4);
 
