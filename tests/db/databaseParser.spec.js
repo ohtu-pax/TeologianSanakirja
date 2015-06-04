352158e2f3124a8222a5740500b561c9ed30b0da
tmakij <timo.j.maki@helsinki.fi>
2015-06-04 14:38:38 +0300

    italics

--- tests/db/databaseParser.spec.js
+++ tests/db/databaseParser.spec.js
@@ -6,7 +6,7 @@ var assert = require('assert');
 describe('lyhenne parseri', function () {
     it('Lisää lyhenteiden selitykset', function (done) {
         var alkuPerainen = 'Tässä on selitys, kr, ja siinä tulisi olla lyhenne';
-        var odotettu = 'Tässä on selitys, <span title="kreikka, kreikan">kr</span>, ja siinä tulisi olla lyhenne';
+        var odotettu = 'Tässä on selitys, <span class="lyhenne" title="kreikka, kreikan">kr</span>, ja siinä tulisi olla lyhenne';
         var tulos = parser.lisaaLyhenne(alkuPerainen);
 
         assert.strictEqual(tulos, odotettu);
@@ -14,7 +14,7 @@ describe('lyhenne parseri', function () {
     });
     it('Lisää lyhenteiden selitykset rajoilla', function (done) {
         var alkuPerainen = 'kr Tässä on selitys, ja siinä tulisi olla lyhenne kr';
-        var odotettu = '<span title="kreikka, kreikan">kr</span> Tässä on selitys, ja siinä tulisi olla lyhenne <span title="kreikka, kreikan">kr</span>';
+        var odotettu = '<span class="lyhenne" title="kreikka, kreikan">kr</span> Tässä on selitys, ja siinä tulisi olla lyhenne <span class="lyhenne" title="kreikka, kreikan">kr</span>';
         var tulos = parser.lisaaLyhenne(alkuPerainen);
 
         assert.strictEqual(tulos, odotettu);
@@ -30,7 +30,7 @@ describe('lyhenne parseri', function () {
     });
     it('Lisää kun ainoa', function (done) {
         var alkuPerainen = 'lat';
-        var odotettu = '<span title="latina, latinan">lat</span>';
+        var odotettu = '<span class="lyhenne" title="latina, latinan">lat</span>';
         var tulos = parser.lisaaLyhenne(alkuPerainen);
 
         assert.strictEqual(tulos, odotettu);
