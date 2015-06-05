5df1d557cdf6f017e71aad75cd84e769b055a267
tmakij <timo.j.maki@helsinki.fi>
2015-06-05 10:47:40 +0300

    selenium suljetaan automaattisesti

--- run_tests.js
+++ run_tests.js
@@ -1,13 +1,13 @@
 'use strict';
 
-var spawn = require('child_process').spawn;
 var childprocess = require('child_process');
+var spawn = childprocess.spawn;
 var exit = process.exit;
 var kill = process.kill;
 
 console.log('Aloitetaan integraatio testit');
 
-var ODOTUS_AIKA = 8000;
+var ODOTUS_AIKA = 3000;
 var TIMEOUT = ODOTUS_AIKA * 15;
 var vastauksiaOnTullut = true;
 
@@ -17,47 +17,30 @@ process.on('exit', cleanup);
 process.on('SIGINT', cleanup);
 process.on('uncaughtException', cleanup);
 
-var seleniumPID = -1;
 var cleaned = false;
 
 var wdm = null;
 var p = null;
 var server = null;
 
-process.stdout.on('data', function (data) {
-    var str = String(data);
-    if (~str.indexOf('seleniumProcess.pid: ')) {
-        var pid = str.replace('seleniumProcess.pid:', '');
-        seleniumPID = parseInt(pid, 10);
-        console.log('TESTS: Selenium pid on ' + seleniumPID);
-    }
-});
-
 server = spawn('node', ['./bin/www'], {
     detached: true,
     stdio: 'inherit'
 });
-//log(server, 'SERVER');
-
-//aikaKatkaisu();
-
-function aikaKatkaisu() {
-    if (vastauksiaOnTullut === false) {
-        console.log('Aikakatkaisu');
-        exit();
-    }
-    vastauksiaOnTullut = false;
-    setTimeout(aikaKatkaisu, TIMEOUT);
-}
 
 setTimeout(function () {
+    if (cleaned === true) {
+        return;
+    }
     wdm = spawn('node_modules/protractor/bin/webdriver-manager', ['start', '--standalone'], {
         detached: true,
         stdio: 'inherit'
     });
-    //log(wdm, 'WEB_DRIVER');
 
     setTimeout(function () {
+        if (cleaned === true) {
+            return;
+        }
         p = spawn('node_modules/protractor/bin/protractor', ['tests/protractor/conf.js'], {
             detached: true,
             stdio: 'inherit'
@@ -66,10 +49,7 @@ setTimeout(function () {
         p.on('SIGINT', quit);
         p.on('uncaughtException', quit);
         p.on('close', quit);
-        /*p.stdout.on('data', function (data) {
-         vastauksiaOnTullut = true;
-         process.stdout.write('PROTRACTOR: ' + data);
-         });*/
+
     }, ODOTUS_AIKA);
 }, ODOTUS_AIKA);
 
@@ -82,7 +62,7 @@ function quit(code) {
 }
 
 function cleanup() {
-    if (cleaned) {
+    if (cleaned === true) {
         return;
     }
     cleaned = true;
@@ -97,25 +77,7 @@ function cleanup() {
     if (p !== null) {
         p.kill();
     }
-    if (seleniumPID !== -1) {
-        console.log("Lopetetaan Selenium");
-        kill(seleniumPID);
-    }
-}
-
-function log(proc, name) {
-    proc.stdout.on('data', function (data) {
-        var str = String(data);
-        process.stdout.write(name + ': ' + str);
-        if (~str.indexOf('seleniumProcess.pid: ')) {
-            var pid = str.replace('seleniumProcess.pid:', '');
-            seleniumPID = parseInt(pid, 10);
-            console.log('TESTS: Selenium pid on ' + seleniumPID);
-        }
-    });
-    proc.on('close', function (code) {
-        if (code) {
-            console.log(name + ' exited with code ' + code);
-        }
-    });
+    var seleniumPID = childprocess.execSync('kill -9 $(lsof -i:4444 -t)');
+    childprocess.execSync('kill', ['-9', seleniumPID]);
+    console.log("Lopetetaan Selenium");
 }
