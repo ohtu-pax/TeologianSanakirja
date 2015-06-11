9c3bda91292edec047a7096a83e0033affb9eeba
Annamaria <ahyotyla@da3-cs-dk108-04.helsinki.fi>
2015-06-11 15:09:22 +0300

    admin: ng-change kuuntelee inputteja --- keskenerainen

--- public/javascripts/controllers/adminController.js
+++ public/javascripts/controllers/adminController.js
@@ -1,12 +1,36 @@
 sanakirjaApp.controller('adminController', function ($scope) {
-    $scope.adminSanaInputs = [{name:'adminSana1'}];
 
+    var eka = {};
+    eka.hakusana = 'asd';
+    eka.id = 0;
+    var toka = {};
+    toka.hakusana = '';
+    toka.id = 6;
+    var objs = [eka, toka];
     
-    $scope.lisaaSanarivi = function () {
-        var newItemNo = $scope.adminSanaInputs.length + 1;
-        $scope.adminSanaInputs.push({name: 'adminSana' + newItemNo});
-          //  console.log(JSON.stringify($scope.adminSanaInputs));
-    }
+    $scope.hsanat = objs;
+    
+    objs[objs.length - 1]; 
+    $scope.onc = function(id){
+        console.log(id);
+    };
+//    $scope.adminSanaInputs = [{name: 'adminSana1'}];
+
+//
+//    console.log(JSON.stringify($scope.adminSanakentta));
+//
+//    $scope.adminSanakentta.adminSana1.watch(function () {
+//        console.log("HELO");
+//    });
+
+//    var testi = angular.element(document.getElementsByName("adminSana1"));
+//    console.log(JSON.stringify(testi));
+
+//    $scope.lisaaSanarivi = function () {
+//        console.log("adminsanakentta " + $scope.adminSanakentta);
+//        var newItemNo = $scope.adminSanaInputs.length + 1;
+//        $scope.adminSanaInputs.push({name: 'adminSana' + newItemNo});
+//    }
 });
 
 
