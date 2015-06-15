5a1df0a53a81dd18096edb45e07a481609c489ab
Annamaria <ahyotyla@da3-cs-dk108-04.helsinki.fi>
2015-06-15 11:15:12 +0300

    admin näkymän muokkausta

--- public/javascripts/controllers/adminController.js
+++ public/javascripts/controllers/adminController.js
@@ -1,5 +1,5 @@
-sanakirjaApp.controller('adminController', function ($scope, sanakirjaAPIservice) {
-    
+sanakirjaApp.controller('adminController', function ($scope, sanakirjaAPIservice, $filter) {
+
     if (sessionStorage.getItem('sanalista') === null) {
         var servicePromise = sanakirjaAPIservice.getSanalista();
 
@@ -12,27 +12,31 @@ sanakirjaApp.controller('adminController', function ($scope, sanakirjaAPIservice
         var sanalista = JSON.parse(sessionStorage.getItem('sanalista'));
         $scope.sanalista = sanalista;
     }
-    
-    $scope.adminSanat = [];
-    
-    $scope.ekaIndeksi = 1;
+
+    $scope.adminSanat = [{id: '1', hakusana: ''}];
 
     $scope.hallinnoiRiveja = function (id) {
-        if ($scope.adminSanat.length === 0) {
-            $scope.adminSanat.push({id: $scope.ekaIndeksi + 1 +""});
-        }else if ((id-1) === $scope.adminSanat.length) {
-            var newItemNo = $scope.adminSanat.length + 2;
-            $scope.adminSanat.push({id: newItemNo});
-        }else if (id === 1 && $scope.adminSanat.length === 1 && $scope.adminSanahaku === '') {
-            $scope.adminSanat = [];                       
-        }else if (id !== 1 && id === $scope.adminSanat.length) {
-            if ($scope.adminSanat[id-2].hakusana === '') {
-                $scope.adminSanat.splice(id-2, 1);
-            }
-        }        
+        //jos toiseksi viimeinen ja se on tyhjä ja sitä seuraava tyhjä, poistetaan vika tyhä
+        if(id === $scope.adminSanat.length - 1 && $scope.adminSanat[id - 1].hakusana === '' && $scope.adminSanat[id].hakusana === '') {
+            $scope.adminSanat.pop();
+            
+        //jos viimeinen sana lisätään tyhjä rivi
+        } else if (id === $scope.adminSanat.length) {
+            var uusiSana = {id: $scope.adminSanat.length + 1 + "", hakusana: ''};
+            $scope.adminSanat.push(uusiSana);
+        }
+    };
+
+    $scope.sanaSelitys = function () {
+        var tulos = $filter('filter')($scope.sanalista, $scope.adminSanahaku, true);
+
+        if (tulos.length === 1) {
+              $scope.adminSelitys =  tulos[0].selitys;
+        } else {
+             $scope.adminSelitys = '';
+        }
     };
 });
-
 
 
 
