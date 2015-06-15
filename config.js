d818768c9e0f3a36f08c389274b26e7a12876f08
tmakij <timo.j.maki@helsinki.fi>
2015-06-15 11:30:08 +0300

    connectionstring config

--- config.js
+++ config.js
@@ -19,4 +19,15 @@ if (password === undefined) {
     config.adminPassword = password;
 }
 
+var connectionString = env.connectionString;
+
+if (connectionString === undefined) {
+    config.connectionString =
+            'postgres://ackprmsigyevpu:Xr2OAYPV1l1GAVW6MMMtVWUhIF'
+            + '@ec2-23-23-81-221.compute-1.amazonaws.com:5432/d4lr4f1ndusjb'
+            + '?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory';
+} else {
+    config.connectionString = connectionString;
+}
+
 module.exports.conf = config;
